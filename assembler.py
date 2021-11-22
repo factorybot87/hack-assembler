fe2893bc79d6c599c6d99fcf440fbde4c75b948b
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-22 20:50:48 +0800

    add Assembler class signature

--- assembler.py
+++ assembler.py
@@ -1,12 +1,26 @@
 import sys
 
+class Assembler:
+    def __init__(self, file: str):
+        """
+        arguments:
+        file: the filename of the hack program
+        """
+        pass
+
+    def convert(self):
+        pass
+
+    def save(self):
+        pass
+
 def main():
     if len(sys.argv) < 2:
         print('Hack assembler')
         print('\nUsage: python3 assembler.py file')
     else:
-        assembler = Assembler()
-        assembler.convert(sys.argv[1])
+        assembler = Assembler(sys.argv[1])
+        assembler.convert()
 
 if __name__ == '__main__':
     main()
