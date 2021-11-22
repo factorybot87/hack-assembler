82bea27f821d9cfff148a93baad181fb0ed6622c
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-22 19:28:47 +0800

    implement dest()

--- code.py
+++ code.py
@@ -1,22 +1,33 @@
 class Code:
-    DEST = {}
+    DEST = {
+        'null': '000',
+        'M':    '001',
+        'D':    '010',
+        'DM':   '011',
+        'MD':   '011',
+        'A':    '100',
+        'AM':   '101',
+        'AD':   '110',
+        'ADM':  '111'
+    }
     COMP = {}
     JUMP = {}
 
-    def dest(symbol: str) -> str:
+    def dest(self, symbol: str) -> str:
         """
-        translates symbol into 3 bits binary code
+        translates symbol into 3 bits binary code.
+        >>> 
+        """
+        return self.DEST[symbol]
+
+    def comp(self, symbol: str) -> str:
+        """
+        translates symbol into 7 bits binary code.
         """
         pass
 
-    def comp(symbol: str) -> str:
+    def jump(self, symbol: str) -> str:
         """
-        translates symbol into 7 bits binary code
-        """
-        pass
-
-    def jump(symbol: str) -> str:
-        """
-        translates symbol into 3 bits binary code
+        translates symbol into 3 bits binary code.
         """
         pass
