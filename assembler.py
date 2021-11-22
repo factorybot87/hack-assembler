f5324134004a00ce0ae7dcef6f7085f33e5fa386
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-22 22:31:40 +0800

    implement convert()

--- assembler.py
+++ assembler.py
@@ -1,18 +1,53 @@
 import sys
 
+from parser import Parser
+from code import Code
+
 class Assembler:
+    WIDTH = 16
+    A_PREFIX = '0'
+    C_PREFIX = '111'
+
     def __init__(self, file: str):
         """
         arguments:
-        file: the filename of the hack program
+        file: the filename of the hack program.
+
+        attribute:
+        file: saves the arguments that has same name.
+        binary_code: hack program in binary representation
         """
-        pass
+        self.file = file
+        self.binary_code = []
 
     def convert(self):
-        pass
+        """
+        converts a hack program to its binary representation.
+        """
+        parser = Parser(self.file)
+        code   = Code()
+        
+        while parser.has_more_lines():
+            parser.advance()
+            if parser.instruction_type() == parser.A_INSTRUCTION:
+                binary = self.A_PREFIX + \
+                         Assembler.convert_decimal_to_15_bits_binary(int(parser.symbol()))
+            elif parser.instruction_type() == parser.C_INSTRUCTION:
+                binary = self.C_PREFIX + \
+                         code.dest(parser.dest()) + \
+                         code.comp(parser.comp()) + \
+                         code.jump(parser.jump())
+            self.binary_code.append(binary)
 
     def save(self):
         pass
+
+    def convert_decimal_to_15_bits_binary(n: int) -> str:
+        """
+        >>> Assembler.convert_decimal_to_15_bits_binary(10)
+        '000000000001010'
+        """
+        return str(bin(n))[2:].zfill(Assembler.WIDTH - len(Assembler.A_PREFIX))
 
 def main():
     if len(sys.argv) < 2:
@@ -23,4 +58,6 @@ def main():
         assembler.convert()
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
     main()
