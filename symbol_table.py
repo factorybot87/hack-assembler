cd1fb141530ead4c218fe5eec2efa5f02a7eb55e
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-23 04:31:58 +0800

    implement constructor

--- symbol_table.py
+++ symbol_table.py
@@ -2,9 +2,33 @@ class SymbolTable:
     def __init__(self):
         """
         attributes:
-        symbols: contains the built-in symbols and the symbolic references.
+        symbol_table: contains the built-in symbols and the symbolic references.
         """
-        pass
+        self.symbol_table = {
+            'R0':     0,
+            'R1':     1,
+            'R2':     2,
+            'R3':     3,
+            'R4':     4,
+            'R5':     5,
+            'R6':     6,
+            'R7':     7,
+            'R8':     8,
+            'R9':     9,
+            'R10':    10,
+            'R11':    11,
+            'R12':    12,
+            'R13':    13,
+            'R14':    14,
+            'R15':    15,
+            'SP':     0,
+            'LCL':    1,
+            'ARG':    2,
+            'THIS':   3,
+            'THAT':   4,
+            'SCREEN': 16384,
+            'KBD':    24576,
+        }
     
     def add_entry(self, symbol: str, address: int):
         """
