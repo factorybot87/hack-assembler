6a6e88b0972474f3027f6228169e6361d831840a
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-22 19:26:44 +0800

    add code class signature

---
+++ code.py
@@ -0,0 +1,22 @@
+class Code:
+    DEST = {}
+    COMP = {}
+    JUMP = {}
+
+    def dest(symbol: str) -> str:
+        """
+        translates symbol into 3 bits binary code
+        """
+        pass
+
+    def comp(symbol: str) -> str:
+        """
+        translates symbol into 7 bits binary code
+        """
+        pass
+
+    def jump(symbol: str) -> str:
+        """
+        translates symbol into 3 bits binary code
+        """
+        pass
