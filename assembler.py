996ab5769a66bb6c20b127ddf8868454810759e5
jerry guo <jerry.kuo@g.ncu.edu.tw>
2021-11-23 16:26:39 +0800

    implement first_pass()

--- assembler.py
+++ assembler.py
@@ -2,6 +2,7 @@ import sys
 
 from parser import Parser
 from code import Code
+from symbol_table import SymbolTable
 
 class Assembler:
     WIDTH = 16
@@ -15,16 +16,43 @@ class Assembler:
 
         attribute:
         file: saves the arguments that has same name.
-        binary_code: hack program in binary representation
+        binary_code: hack program in binary representation.
+        symbol_table: contains the built-in symbols and the symbolic references.
         """
         self.file = file
         self.binary_code = []
+        self.symbol_table = SymbolTable()
 
     def first_pass(self):
         """
         builds a symbol table that contains all the label symbols.
+
+        >>> a.first_pass()
+        >>> a.symbol_table.contains('OUTPUT_D')
+        True
+        >>> a.symbol_table.get_address('OUTPUT_D')
+        16
+        >>> a.symbol_table.contains('OUTPUT_FIRST')
+        True
+        >>> a.symbol_table.get_address('OUTPUT_FIRST')
+        2
         """
-        pass
+        parser = Parser(self.file)
+        line_nunber_counter = -1
+        next_available_address = 16
+
+        while parser.has_more_lines():
+            parser.advance()
+            if parser.instruction_type() == parser.L_INSTRUCTION:
+                self.symbol_table.add_entry(parser.symbol(), line_nunber_counter + 1)
+            else:
+                line_nunber_counter += 1
+                if parser.instruction_type() == parser.A_INSTRUCTION:
+                    if not parser.symbol().isnumeric() and not self.symbol_table.contains(parser.symbol()):
+                        self.symbol_table.add_entry(parser.symbol(), next_available_address)
+                        next_available_address += 1
+            
+                
 
     def second_pass(self):
         """
